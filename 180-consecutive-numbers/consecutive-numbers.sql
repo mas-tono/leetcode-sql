bcd1784ff90a071359ea1b470a11b07e83983f81
Hartono <124220894+mas-tono@users.noreply.github.com>
2024-09-01 18:43:37 +0700

    Time: 485 ms (98.04%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -1,11 +1,20 @@
 /* Write your T-SQL query statement below */
 
-select distinct num as ConsecutiveNums 
+-- check the 'id' too, it's not really autoincrement
+
+select distinct num as ConsecutiveNums
 from (
-	select *, case
-		when (num = lagg) and (num = leadd) then 1 else 0
-		end as cek
-	from (
-		select *, LAG(num) over(order by id) as lagg, LEAD(num) over(order by id) as leadd
-		from Logs) l) c
-where cek = 1
+    select *, 
+            case 
+                when (num = lagg_num) and (num = leadd_num) then 1 else 0
+            end as cek_num,
+            case
+                when leadd_id - lagg_id = 2 then 1 else 0
+            end as cek_id
+    from (select *, 
+                lag(num) over(order by id) as lagg_num,             
+                lead(num) over(order by id) as leadd_num,
+                lag(id) over(order by id) as lagg_id,
+                lead(id) over(order by id) as leadd_id
+    from Logs) c) d
+where cek_num = 1 and cek_id = 1
